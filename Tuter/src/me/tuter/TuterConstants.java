5b78208f70606a62fff6f3980a7c1f861b188be5
Sher Minn Chong <schong7@wisc.edu>
2013-05-10 17:27:01 -0500

    Add Post activity

--- Tuter/src/me/tuter/TuterConstants.java
+++ Tuter/src/me/tuter/TuterConstants.java
@@ -1,8 +1,10 @@
 package me.tuter;
 
 public class TuterConstants {
-	public static final String DOMAIN = "http://tuter.herokuapp.com";
+//	public static final String DOMAIN = "http://tuter.herokuapp.com";
 //	public static final String DOMAIN = "http://72.33.184.242:3000";
+	public static final String DOMAIN = "http://72.33.236.32:3000";
+
 	public static final String URI_SEARCH= "/search.json";
 
 	public static final int READ_TIMEOUT 	= 6000;
---
+++ Tuter/src/me/tuter/tasks/GetSearchResultsParamsTask.java
@@ -0,0 +1,6 @@
+//Post request to server to get search results with params
+package me.tuter.tasks;
+
+public class GetSearchResultsParamsTask {
+
+}
